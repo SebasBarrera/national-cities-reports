e4f86602be3642242c282fc2b5572f777ccf293c
Juan Sebastian Barrera Pulido <1144105003@u.icesi.edu.co>
2021-02-07 14:32:03 -0500

    dataset has been charged

--- Project_1/Program.cs
+++ Project_1/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Project_1.model;
+using System.IO;
 
 namespace Project_1
 {
@@ -23,7 +24,18 @@ namespace Project_1
             Console.WriteLine("holamundo");
             Console.WriteLine(x.Type);
 
+            String[] lines = File.ReadAllLine("./data/dataset.csv");
 
+            foreach (String line in lines) {
+            
+                String values = line.Split(',');
+
+                String codDpto = values[0];
+                String codMpio = values[1];
+                String dpto = values[2];
+                String mpio = values[3];
+                String tipo = values[4];
+            }
         }
     }
 }
