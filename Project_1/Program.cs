f753c104d91f9fe62cebc02a0a8afd9a9984a01d
Juan Sebastian Barrera Pulido <1144105003@u.icesi.edu.co>
2021-02-07 15:44:57 -0500

    The load of the dataset has moved to Dane.cs

--- Project_1/Program.cs
+++ Project_1/Program.cs
@@ -20,21 +20,8 @@ namespace Project_1
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
 
-            Municipality x = new Municipality("1","2","3","4","5");
-          //arreglar
-
-            string[] lines = File.ReadAllLines("./data/dataset.csv");
-
-            foreach (String line in lines) {
             
-                string[] values = line.Split(',');
-
-                string codDpto = values[0];
-                string codMpio = values[1];
-                string dpto = values[2];
-                string mpio = values[3];
-                string type = values[4];
-            }
+            
         }
     }
 }
---
+++ Project_1/model/Dane.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace Project_1.model
+{
+    class Dane
+    {
+        private Municipality municipalities = new ArrayList();
+
+        public Dane()
+        {
+
+        }
+
+        public loadData()
+        {
+            string[] lines = File.ReadAllLines("./data/dataset.csv");
+
+
+
+            foreach (String line in lines)
+            {
+
+                string[] values = line.Split(',');
+
+                string codDpto = values[0];
+                string codMpio = values[1];
+                string dpto = values[2];
+                string mpio = values[3];
+                string type = values[4];
+
+
+                municipalities.add(new Municipality(codDpto, codMpio, dpto, mpio, type));
+            }
+        }
+
+        public ArrayList<Municipality> municipalities
+        {
+            get; set;
+        }
+
+        public Arraylist<Municipality> selectedByChar(char x)
+        {
+             Municipality selected = new ArrayList();
+            foreach (Municipality i in municipalities)
+            {
+                if (municipalities[i].charAt(0).Equals(x)
+                {
+                    selected.add(municipalities[i]);
+                }
+            }
+        }
+    }
+}
